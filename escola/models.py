9d26c9d75ac51ce92cc41135b6b8967a1b76dabf
NiRovil <nicolas199851@hotmail.com>
2022-04-17 18:27:33 -0300

    Definindo as URLs, viewsets e cadastro

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30)
+    idade = models.CharField(max_length=3)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    data_de_nascimento = models.DateField()
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL = (
+        ('A', 'Avançado'),
+        ('B', 'Básico'),
+        ('I', 'Intermediário')
+    )
+    curso = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=50)
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
